2051980f22973680d9a2011c7a13fdc1491caa50
Intissar99 <meddebintissar@gmail.com>
2025-06-17 06:58:06 +0200

    propose mission methode added

--- src/main/scala/controllers/MissionController.scala
+++ src/main/scala/controllers/MissionController.scala
@@ -1,6 +1,9 @@
 package controllers
+case class Mission(description: String, xpReward: Int)
 
-case class Mission(description: String, xpReward: Int)  
+enum MissionDecision:
+  case Accept, Refuse
+
 
 object MissionController {
   def start(player: Player, mission: Mission): (Player, String) = {
@@ -11,4 +14,12 @@ object MissionController {
     val updatedPlayer = PlayerController.gainXP(player, mission.xpReward) 
     (updatedPlayer, s"${player.name} completed the mission: ${mission.description}")
   }
+
+  def proposeMission(player: Player, mission: Mission): (Player, MissionDecision) = {
+    val accepted = scala.util.Random.nextBoolean()
+    val decision = if accepted then MissionDecision.Accept else MissionDecision.Refuse
+    println(s"${player.name} ${if accepted then "accepte" else "refuse"} la mission : ${mission.description}")
+    (player, decision)
+  }
+
 }
