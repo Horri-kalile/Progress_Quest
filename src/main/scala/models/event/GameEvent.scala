05e610b7d25feeeaba5185c622e50641ba8c28dd
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-07-07 22:21:47 +0200

    udpated wrong training event exp

--- src/main/scala/models/event/GameEvent.scala
+++ src/main/scala/models/event/GameEvent.scala
@@ -39,7 +39,7 @@ case object MissionEvent extends GameEvent:
 
 case object TrainingEvent extends GameEvent:
   override def action(player: Player): (Player, List[String], Option[Monster]) =
-    val exp = Random.between((player.exp * 0.25).toInt, (player.exp * 0.5).toInt)
+    val exp = player.level * Random.between(1, 100)
     val msg = s"Training completed: +$exp EXP"
     println(msg)
     (player.gainExp(exp), List(msg), None)
