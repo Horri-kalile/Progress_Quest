21d36a9bb284dcfe1b4c81f95f92f4f48fab2e1e
Horri kalile <103006189+Horri-kalile@users.noreply.github.com>
2025-07-11 13:19:45 -0600

    create SpecialEventDialog with blessing/curse popup and 5s timer

---
+++ src/main/scala/view/SpecialEventDialog.scala
@@ -0,0 +1,56 @@
+package view
+
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Alert, ButtonType}
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.application.Platform
+import java.util.{Timer, TimerTask}
+
+object SpecialEventDialog:
+  
+  /**
+   * Show blessing/curse dialog with 5-second auto-ignore timer
+   * @return Some(true) if player chooses to pray, Some(false) if ignore, None if timeout
+   */
+  def showBlessingCurseDialog(): Option[Boolean] =
+    showTimedDialog(
+      title = "Mysterious Shrine",
+      header = "You encounter a mysterious shrine...",
+      content = "Do you want to pray at the shrine?\n(Could be a blessing or a curse!)",
+      yesText = "Pray",
+      noText = "Ignore"
+    )
+  
+  /**
+   * Private helper method to show timed dialog with 5-second auto-close
+   */
+  private def showTimedDialog(
+    title: String, 
+    header: String, 
+    content: String, 
+    yesText: String, 
+    noText: String
+  ): Option[Boolean] =
+    val dialog = new Alert(AlertType.Confirmation):
+      this.title = title
+      headerText = header
+      contentText = s"$content\n\n⏰ Auto-ignore in 5 seconds..."
+      
+    val yesButton = new ButtonType(yesText, ButtonData.YesButton)
+    val noButton = new ButtonType(noText, ButtonData.NoButton)
+    dialog.buttonTypes = Seq(yesButton, noButton)
+    
+    // 5-second timer for auto-close
+    val timer = new Timer()
+    timer.schedule(new TimerTask {
+      def run(): Unit = Platform.runLater(() => dialog.close())
+    }, 5000)
+    
+    val result = dialog.showAndWait()
+    timer.cancel() // Cancel timer if user made a choice
+    
+    result match
+      case Some(`yesButton`) => Some(true)   // Player chose "yes" action
+      case Some(`noButton`) => Some(false)   // Player chose "no" action  
+      case None => None                      // Timed out = auto-ignore
+
