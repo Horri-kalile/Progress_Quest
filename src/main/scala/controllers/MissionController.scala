6f2d62d599d1fcf6f654c0c608518abe8b539913
Intissar99 <meddebintissar@gmail.com>
2025-07-15 03:18:27 +0200

    Structure updated

--- src/main/scala/controllers/MissionController.scala
+++ src/main/scala/controllers/MissionController.scala
@@ -1,1 +1,39 @@
+package controllers
+
+import models.event.{Mission, MissionFactory}
+import models.player.Player
+import models.player.Item
+import controllers.PlayerController
+
+
+object MissionController:
+
+  def createRandomMission(): Mission =
+    MissionFactory.randomMission()
+
+
+  def progressMission(mission: Mission): Mission =
+    mission.progressed()
+
+
+  def isCompleted(mission: Mission): Boolean =
+    mission.isCompleted
+
+
+  def completeMission(player: Player, mission: Mission): Player =
+    var updatedPlayer = PlayerController.gainXP(player, mission.rewardExp)
+
+    mission.rewardGold.foreach { gold =>
+      updatedPlayer = updatedPlayer.earnGold(gold)
+    }
+
+    mission.rewardItem.foreach { item =>
+      updatedPlayer = PlayerController.addItem(updatedPlayer, item)
+
+    }
+
+    updatedPlayer
+
+
+
 
